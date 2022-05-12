46db20b7f51082dbd1e0e1d14b8b49dc61ed7bd2
santanarscs <raphaelstn@gmail.com>
2022-05-12 11:15:11 -0300

    configure angular material

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    AuthLayoutComponent,
+    MainLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
